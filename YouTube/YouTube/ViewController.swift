530db1e09452d9527960b037487d56539a2b5170
MAC <mac@Air-cua-MAC.net.fpt>
2018-01-29 23:03:40 +0700

    init

---
+++ YouTube/YouTube/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  YouTube
+//
+//  Created by MAC on 1/29/18.
+//  Copyright © 2018 MAC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.red
+    }
+}
+
