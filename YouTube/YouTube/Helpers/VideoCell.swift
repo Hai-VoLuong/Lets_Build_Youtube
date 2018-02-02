cd05983ec32b85d73a24959aed3be8447eb11c0a
MAC <mac@Air-cua-MAC.net.fpt>
2018-02-02 21:45:18 +0700

    create collectionView Bar

--- YouTube/YouTube/Helpers/VideoCell.swift
+++ YouTube/YouTube/Helpers/VideoCell.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import LBTAComponents
 
 class VideoCell: UICollectionViewCell {
     
---
+++ YouTube/YouTube/View/MenuBar.swift
@@ -0,0 +1,56 @@
+//
+//  MenuBar.swift
+//  YouTube
+//
+//  Created by MAC on 2/2/18.
+//  Copyright © 2018 MAC. All rights reserved.
+//
+
+import UIKit
+
+class MenuBar: UIView, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.backgroundColor = UIColor(r: 230, g: 32, b: 31)
+        cv.dataSource = self
+        cv.delegate = self
+        return cv
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
+        addSubview(collectionView)
+        addConstraintsWithFormat("H:|[v0]|", views: collectionView)
+        addConstraintsWithFormat("V:|[v0]|", views: collectionView)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        cell.backgroundColor = .blue
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: frame.width / 4, height: frame.height)
+    }
+    
+    // khoang cach giua cac item
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+
