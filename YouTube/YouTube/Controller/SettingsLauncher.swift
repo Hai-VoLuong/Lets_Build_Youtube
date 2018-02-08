3ed45257b3c1bf245dbdf957abb26e2fafa471cb
MAC <mac@Air-cua-MAC.net.fpt>
2018-02-09 00:31:42 +0700

    dismiss collectionView

--- YouTube/YouTube/Controller/SettingsLauncher.swift
+++ YouTube/YouTube/Controller/SettingsLauncher.swift
@@ -14,26 +14,51 @@ final class SettingsLauncher {
     
     private let bag = DisposeBag()
     private let blackView = UIView()
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.backgroundColor = .white
+        return cv
+    }()
 
     func showSettings() {
-        let tapGesture = UITapGestureRecognizer()
-        blackView.addGestureRecognizer(tapGesture)
-        tapGesture.rx.event.bind(onNext: { [weak self] _ in
-            guard let this = self else { return }
-            UIView.animate(withDuration: 0.5, animations: {
-                this.blackView.alpha = 0
-            })
-        }).disposed(by: bag)
+       
+        handleDismiss()
         
         if let window = UIApplication.shared.keyWindow {
             blackView.backgroundColor = UIColor(white: 0, alpha: 0.5)
             window.addSubview(blackView)
+            window.addSubview(collectionView)
+            let height: CGFloat = 200
+            let y = window.frame.height - height
+            collectionView.frame = CGRect(x: 0, y: window.frame.height, width: window.frame.width, height: height)
+            
             blackView.frame = window.frame
             blackView.alpha = 0
-            UIView.animate(withDuration: 0.5, animations: {
+            
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
                 self.blackView.alpha = 1
-            })
+        
+                self.collectionView.frame = CGRect(x: 0, y: y, width: self.collectionView.frame.width, height: self.collectionView.frame.height)
+            }, completion: nil)
         }
+    }
+    
+    private func handleDismiss() {
+        let tapGesture = UITapGestureRecognizer()
+        blackView.addGestureRecognizer(tapGesture)
+        tapGesture.rx.event.bind(onNext: { [weak self] _ in
+        guard let this = self else { return }
+        UIView.animate(withDuration: 0.5, animations: {
+                this.blackView.alpha = 0
+                if let window = UIApplication.shared.keyWindow {
+                    self?.collectionView.frame = CGRect(x: 0,
+                    y: window.frame.height,
+                    width: (self?.collectionView.frame.width)!,
+                    height: (self?.collectionView.frame.height)!)
+                }
+            })
+        }).disposed(by: bag)
     }
     
     init() {
