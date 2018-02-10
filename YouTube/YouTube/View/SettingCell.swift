9905d87e054d628f504e7536cd3035a4f1e289cd
MAC <mac@Air-cua-MAC.net.fpt>
2018-02-10 22:35:21 +0700

    update image

--- YouTube/YouTube/View/SettingCell.swift
+++ YouTube/YouTube/View/SettingCell.swift
@@ -17,11 +17,30 @@ final class SettingCell: BaseCell {
         return label
     }()
     
+    let iconImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = #imageLiteral(resourceName: "settings")
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
     override func setupViews() {
         super.setupViews()
         addSubview(nameLabel)
+        addSubview(iconImageView)
         
-        // 8'30 ep8
-        nameLabel.anchor(topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
+        nameLabel.anchor(topAnchor, left: iconImageView.rightAnchor, bottom: bottomAnchor, right: rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
+        
+        iconImageView.anchor(topAnchor, left: leftAnchor, bottom: bottomAnchor, right: nameLabel.leftAnchor, topConstant: 8, leftConstant: 25, bottomConstant: 8, rightConstant: 25, widthConstant: 5, heightConstant: 5)
     }
 }
+
+
+
+
+
+
+
+
+
+
